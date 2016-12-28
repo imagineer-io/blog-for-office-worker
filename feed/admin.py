dd07fad74d30a0f62ca3ad65addfe61b5f49c0b0
Minsoo Kwak <marcosdeseul@gmail.com>
2016-12-28 13:40:47 +0800

    Add migrations and admin settings

--- feed/admin.py
+++ feed/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Article, Comment, HashTag
 # Register your models here.
+@admin.register(Article, Comment, HashTag)
+class FeedAdmin(admin.ModelAdmin):
+    pass
--- feed/models.py
+++ feed/models.py
@@ -17,10 +17,19 @@ class Article(models.Model):
         default=DEVELOPMENT,
     )
 
+    def __str__(self):
+        return self.title
+
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     username = models.CharField(max_length=50)
     content = models.CharField(max_length=200)
 
+    def __str__(self):
+        return "{}에 댓글: {}".format(self.article.title, self.content)
+
 class HashTag(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
