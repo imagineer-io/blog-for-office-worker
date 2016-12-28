228f3c314ca0b993aa046ce28105d8ae3fc8dcd2
Minsoo Kwak <marcosdeseul@gmail.com>
2016-12-28 14:44:39 +0800

    Decorate with bootstrap
    - filled index.html, detail.html
    - separated navbar.html from those 2 html files
    - copied css from bootstrap example

--- feed/urls.py
+++ feed/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    # url(r'^$', views.detail, name='index'),
+    url(r'^(?P<article_id>[0-9]+)/$', views.detail, name='index'),
     # url(r'^$', views.about, name='index'),
 ]
--- feed/views.py
+++ feed/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
+from .models import Article, Comment, HashTag
 
 # Create your views here.
 def index(request):
-    pass
+    article_list = Article.objects.all()
+    hashtag_list = HashTag.objects.all()
+    ctx = {
+        "article_list" : article_list,
+        "hashtag_list" : hashtag_list,
+    }
+    return render(request, "index.html", ctx)
 
-def detail(request):
-    pass
+def detail(request, article_id):
+    article = Article.objects.get(id=article_id)
+    hashtag_list = HashTag.objects.all()
+    ctx = {
+        "article" : article,
+        "hashtag_list" : hashtag_list,
+    }
+    return render(request, "detail.html", ctx)
 
 # def about(request):
-#     pass    
+#     pass
